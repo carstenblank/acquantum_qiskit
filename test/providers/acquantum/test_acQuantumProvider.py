e22a128faf534adf73d97e33aa02f9fead06de37
Sebastian Boerakker <basti@boerakker.com>
2019-01-16 14:38:55 +0100

    update unit tests

--- test/providers/acquantum/test_acQuantumProvider.py
+++ test/providers/acquantum/test_acQuantumProvider.py
@@ -8,14 +8,19 @@ class TestAcQuantumProvider(TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        os.environ['ACQ_USER'] = 'sebboer'
-        os.environ['ACQ_PWD'] = 'qnpwzHyIIFw33Nw2PBx'
 
     def test_load_account(self):
         self.provider = AcQuantumProvider()
         self.provider.load_account()
+        accounts = self.provider._accounts
+        self.assertTrue(os.environ['ACQ_USER'] in accounts)
 
     def test_get_backends(self):
         self.provider = AcQuantumProvider()
         self.provider.load_account()
-        self.assertEqual(2, len(self.provider.backends()))
+        backends = self.provider.backends()
+        self.assertEqual(2, len(backends))
+        sim, _ = backends[0]
+        real, _ = backends[1]
+        self.assertEqual('SIMULATE', sim)
+        self.assertEqual('REAL', real)
--- test/providers/acquantum/test_acQuantumSingleProvider.py
+++ test/providers/acquantum/test_acQuantumSingleProvider.py
@@ -1,5 +1,8 @@
+import os
 from unittest import TestCase
+from unittest.mock import Mock
 
+from providers.acquantum.acquantumsingleprovider import AcQuantumSingleProvider
 from providers.acquantum.credentials import AcQuantumCredentials
 
 
@@ -7,7 +10,18 @@ class TestAcQuantumSingleProvider(TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self._cred = AcQuantumCredentials('sebboer', 'qnpwzHyIIFw33Nw2PBx')
+        self._cred = AcQuantumCredentials(os.environ['ACQ_USER'], os.environ['ACQ_PWD'])
 
     def test_init(self):
         pass
+
+    def test_backends(self):
+        provider = Mock()
+
+        single_provider = AcQuantumSingleProvider(self._cred, provider)
+        backends = list(single_provider.backends())
+        self.assertEqual(len(backends), 2)
+        sim, _ = backends[0]
+        real, _ = backends[1]
+        self.assertEqual('SIMULATE', sim)
+        self.assertEqual('REAL', real)
