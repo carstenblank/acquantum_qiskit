e04939083f4a2abd1a5ed63f7623d919a4d25f4e
Sebastian Boerakker <basti@boerakker.com>
2019-01-16 14:51:32 +0100

    update unit tests

--- test/providers/acquantum/credentials/test_credentials.py
+++ test/providers/acquantum/credentials/test_credentials.py
@@ -9,27 +9,35 @@ class TestCredentials(TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        os.environ['ACQ_USER'] = 'test'
-        os.environ['ACQ_PWD'] = 'secure'
 
     def test_env_reading(self):
         cred = read_credentials_from_environ()
-        self.assertEqual(cred.user_name, 'test')
-        self.assertEqual(cred.password, 'secure')
+        self.assertEqual(cred.user_name, os.environ['ACQ_USER'])
+        self.assertEqual(cred.password, os.environ['ACQ_PWD'])
 
     def test_discover_credentials(self):
         cred = discover_credentials()
-        self.assertEqual(cred.password, 'secure')
-        self.assertEqual(cred.user_name, 'test')
+        self.assertEqual(cred.user_name, os.environ['ACQ_USER'])
+        self.assertEqual(cred.password, os.environ['ACQ_PWD'])
 
     def test_no_credentials_discover(self):
+        user = os.environ['ACQ_USER']
+        pwd = os.environ['ACQ_PWD']
         del os.environ['ACQ_USER']
         del os.environ['ACQ_PWD']
         with self.assertRaises(AcQuantumAccountError):
             discover_credentials()
 
+        os.environ['ACQ_USER'] = user
+        os.environ['ACQ_PWD'] = pwd
+
     def test_no_credentials_from_environ(self):
+        user = os.environ['ACQ_USER']
+        pwd = os.environ['ACQ_PWD']
         del os.environ['ACQ_USER']
         del os.environ['ACQ_PWD']
         with self.assertRaises(AcQuantumAccountError):
             read_credentials_from_environ()
+
+        os.environ['ACQ_USER'] = user
+        os.environ['ACQ_PWD'] = pwd
