ffd5b2bb3f9d8c98b5c081926eda672064a55426
Sebastian Boerakker <basti@boerakker.com>
2019-01-23 10:33:47 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="acquantum_qiskit",
+    version="0.0.1",
+    author="Carsten Blank",
+    author_email="blank@data-cybernetics.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="rst",
+    url="https://github.com/sebboer/acquantum_qiskit",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
