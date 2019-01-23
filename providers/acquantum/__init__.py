5586d20c622bdd0b8e65e74629fd61c0d2f6bb6e
Sebastian Boerakker <basti@boerakker.com>
2019-01-23 12:07:25 +0100

    update package init

---
+++ providers/acquantum/__init__.py
@@ -0,0 +1,1 @@
+from .acquantumbackend import AcQuantumBackend
---
+++ providers/acquantum/models/__init__.py
@@ -0,0 +1,4 @@
+from .Gates import XGate, YGate, ZGate, SGate, HGate, RxGate, RyGate, Measure, CPhase, CCPhase
+from .Model import AcQuantumRequestError, AcQuantumRequestForbiddenError, AcQuantumResultResponse, AcQuantumResult, \
+    AcQuantumExperimentDetail, \
+    AcQuantumExperiment, AcQuantumResponse, AcQuantumErrorResponse
