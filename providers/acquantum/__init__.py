7bea3b0b9a5c580dd3142ce3d82b0e4554322a7e
Sebastian Boerakker <basti@boerakker.com>
2019-01-23 12:02:46 +0100

    update __init__.py

--- providers/acquantum/__init__.py
+++
@@ -1,6 +0,0 @@
-from .acquantumbackend import AcQuantumBackend
-from .acquantumjob import AcQuantumJob, AcQuantumJobStatus
-from .acquantumprovider import AcQuantumProvider
-from .acquantumsingleprovider import AcQuantumSingleProvider
-from .backendconfiguration import AcQuantumBackendConfiguration, AcQuantumBackendConfigurationSchema, \
-    AcQuantumGateConfig, AcQuantumGateConfigSchema
--- providers/acquantum/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Gates import XGate, YGate, ZGate, SGate, HGate, RxGate, RyGate, Measure, CPhase, CCPhase
-from .Model import AcQuantumRequestError, AcQuantumRequestForbiddenError, AcQuantumResultResponse, AcQuantumResult, \
-    AcQuantumExperimentDetail, \
-    AcQuantumExperiment, AcQuantumResponse, AcQuantumErrorResponse
