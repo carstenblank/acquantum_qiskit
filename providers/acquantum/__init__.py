1e11b8f506d668f40056b84333498bf5cad254dc
Sebastian Boerakker <basti@boerakker.com>
2019-01-23 11:43:13 +0100

    udpate __init__.py

---
+++ providers/acquantum/__init__.py
@@ -0,0 +1,6 @@
+from .acquantumbackend import AcQuantumBackend
+from .acquantumjob import AcQuantumJob, AcQuantumJobStatus
+from .acquantumprovider import AcQuantumProvider
+from .acquantumsingleprovider import AcQuantumSingleProvider
+from .backendconfiguration import AcQuantumBackendConfiguration, AcQuantumBackendConfigurationSchema, \
+    AcQuantumGateConfig, AcQuantumGateConfigSchema
