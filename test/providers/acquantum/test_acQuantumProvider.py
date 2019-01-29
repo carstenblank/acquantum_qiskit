b6f0281484484fcd2b8d757bcaa4d816497505a7
Sebastian Boerakker <basti@boerakker.com>
2019-01-29 15:14:01 +0100

    optimize add mocks for api calls

--- test/providers/acquantum/test_acQuantumProvider.py
+++ test/providers/acquantum/test_acQuantumProvider.py
@@ -1,7 +1,26 @@
 import os
-from unittest import TestCase
+from unittest import TestCase, mock
 
+from providers.acquantum.acquantumerrors import AcQuantumBackendError
 from providers.acquantum.acquantumprovider import AcQuantumProvider
+
+
+class SingleMock:
+
+    def __init__(self, credentials, provider):
+        self.provider = provider
+        self.credentials = credentials
+
+    def authenticate(self):
+        print(self.credentials)
+        print('auth called')
+
+    def backends(self, name=None, **kwargs):
+        print('backends called')
+        if self.credentials:
+            return [mock.Mock(), mock.Mock()]
+        else:
+            return []
 
 
 class TestAcQuantumProvider(TestCase):
@@ -9,18 +28,25 @@ class TestAcQuantumProvider(TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-    def test_load_account(self):
+    @mock.patch('providers.acquantum.acquantumprovider.AcQuantumSingleProvider', side_effect=SingleMock)
+    def test_load_account(self, single_prov_mock):
         self.provider = AcQuantumProvider()
         self.provider.load_account()
         accounts = self.provider._accounts
         self.assertTrue(os.environ['ACQ_USER'] in accounts)
+        single_prov_mock.assert_called()
+        single_prov_mock.backends.assert_not_called()
 
-    def test_get_backends(self):
+    @mock.patch('providers.acquantum.acquantumprovider.AcQuantumSingleProvider', side_effect=SingleMock)
+    def test_get_backends(self, single_prov_mock):
         self.provider = AcQuantumProvider()
         self.provider.load_account()
         backends = self.provider.backends()
         self.assertEqual(2, len(backends))
-        sim, _ = backends[0]
-        real, _ = backends[1]
-        self.assertEqual('SIMULATE', sim)
-        self.assertEqual('REAL', real)
+
+    @mock.patch('providers.acquantum.acquantumprovider.AcQuantumSingleProvider', side_effect=SingleMock)
+    def test_get_backends_without_loading_accounts(self, single_prov_mock):
+        provider = AcQuantumProvider()
+        with self.assertRaises(AcQuantumBackendError):
+            provider.backends()
+            single_prov_mock.backends.assert_called_once()
--- test/providers/acquantum/test_acQuantumSingleProvider.py
+++ test/providers/acquantum/test_acQuantumSingleProvider.py
@@ -1,9 +1,51 @@
 import os
-from unittest import TestCase
+from unittest import TestCase, mock
 from unittest.mock import Mock
 
 from acquantumconnector.credentials.credentials import AcQuantumCredentials
 from providers.acquantum.acquantumsingleprovider import AcQuantumSingleProvider
+
+backends_config = [
+    {
+        'backend_name': 'SIMULATE',
+        'backend_version': '0.0.1',
+        'n_qubits': 25,
+        'basis_gates': ['XGate'],
+        'gates': [
+            {'name': 'u1', 'parameters': ['lambda'], 'qasm_def': 'gate u1(lambda) q { U(0,0,lambda) q; }'},
+            {'name': 'u2', 'parameters': ['phi', 'lambda'],
+             'qasm_def': 'gate u2(phi,lambda) q { U(pi/2,phi,lambda) q; }'},
+            {'name': 'u3', 'parameters': ['theta', 'phi', 'lambda'],
+             'qasm_def': 'gate u3(theta,phi,lambda) q { U(theta,phi,lambda) q; }'},
+            {'name': 'cx', 'parameters': ['c', 't'], 'qasm_def': 'gate cx c,t { CX c,t; }'},
+            {'name': 'id', 'parameters': ['a'], 'qasm_def': 'gate id a { U(0,0,0) a; }'},
+            {'name': 'snapshot', 'parameters': ['slot'], 'qasm_def': 'gate snapshot(slot) q { TODO }'}
+        ],
+        'local': False,
+        'simulator': True,
+        'conditional': False,
+        'open_pulse': False,
+        'memory': False,
+        'max_shots': 8192
+    },
+    {
+        'backend_name': 'REAL',
+        'backend_version': '0.0.1',
+        'n_qubits': 11,
+        'basis_gates': ['XGate'],
+        'gates': [
+            {'name': 'x',
+             'parameters': [''],
+             'qasm_def': ''}
+        ],
+        'local': False,
+        'simulator': False,
+        'conditional': False,
+        'open_pulse': False,
+        'memory': False,
+        'max_shots': 20000
+    }
+]
 
 
 class TestAcQuantumSingleProvider(TestCase):
@@ -12,10 +54,19 @@ class TestAcQuantumSingleProvider(TestCase):
         super().setUp()
         self._cred = AcQuantumCredentials(os.environ['ACQ_USER'], os.environ['ACQ_PWD'])
 
-    def test_init(self):
-        pass
+    @mock.patch('providers.acquantum.acquantumsingleprovider.AcQuantumConnector.create_session')
+    @mock.patch('providers.acquantum.acquantumsingleprovider.AcQuantumConnector.available_backends',
+                return_value=backends_config)
+    def test_init(self, available_backends, create_session):
+        provider = Mock()
+        AcQuantumSingleProvider(self._cred, provider)
+        create_session.assert_called_once_with(self._cred)
+        available_backends.assert_called_once()
 
-    def test_backends(self):
+    @mock.patch('providers.acquantum.acquantumsingleprovider.AcQuantumConnector.create_session')
+    @mock.patch('providers.acquantum.acquantumsingleprovider.AcQuantumConnector.available_backends',
+                return_value=backends_config)
+    def test_backends(self, available_backends, create_session):
         provider = Mock()
 
         single_provider = AcQuantumSingleProvider(self._cred, provider)
@@ -25,3 +76,5 @@ class TestAcQuantumSingleProvider(TestCase):
         real, _ = backends[1]
         self.assertEqual('SIMULATE', sim)
         self.assertEqual('REAL', real)
+        create_session.assert_called_once_with(self._cred)
+        available_backends.assert_called_once()
